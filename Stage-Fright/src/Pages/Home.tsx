7809570e64baf081f9d440ced74495968eed6dcb
Kevin Diaz <kevindiaz.08.21.07@gmail.com>
2024-12-10 11:59:10 -0600

    .

--- Stage-Fright/src/Pages/Home.tsx
+++ Stage-Fright/src/Pages/Home.tsx
@@ -1,11 +1,19 @@
 import './Home.css'
+import album from '/album cover.jpg'
+import {Button} from './Button'
 export function Home(){
     return(
         <>
             <div id='content'>
             <div id='Banner'></div>
-            <div id='Releases'><h1>R E L E A S E S</h1></div>
-            <div id='Merch'><h1>T O P S E L L E R S</h1></div>
+            <div id='Releases'>
+                <h1>R E L E A S E S</h1>
+                <img id='album'src={album} alt='album cover'/>
+            </div>
+            <div id='Merch'>
+                <h1>T O P S E L L E R S</h1>
+                {Button()}
+            </div>
             </div>
             
         </>
